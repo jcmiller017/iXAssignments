3a2978041df7e62456c4cf47e89f8a78549ceceb
Joanna Miller <joannamiller@Joannas-MacBook-Pro.local>
2016-06-07 21:33:17 +0200

    Day7 afternoon

---
+++ Day7/day7afternoon.js
@@ -0,0 +1,47 @@
+// change the color of the text in the div with class "color-div" to red
+function changeColor() {
+	$(".color-div").css('color', "red");
+}
+
+// add the word "blue" to the div with class "add-div"
+function addToDiv() {
+	$(".add-div").append("blue");
+}
+
+// add a title to the page
+function addTitle() {
+	$(".title-div").append("<h1> iXperience Fun <h1>");
+}
+
+// alert the text in the selected item of the dropdown
+function getSelected() {
+	alert($("#selector option:selected").text());
+}
+
+// change the title to be "New Title"
+function changeText() {
+	$(".title-div").replaceWith("<h1> New Title <h1>")
+}
+
+$(document).ready(function() {
+
+addToDiv();
+addTitle();
+$('.get-selected').click(function(){
+	getSelected();
+})
+
+  $('.the-button').click(function(){ 
+    changeColor();
+	});
+
+  $('.text-changer').click(function(){
+  	changeText();
+  })
+	
+  $(".trio").click(function (e) {
+    $(".clicked-id").html("you clicked on id:" + $(this).attr("id"));
+  });
+
+});
+
