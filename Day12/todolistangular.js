0e7876426e32ed1734a95f97df3ce0c96c0e5551
Joanna Miller <joannamiller@Joannas-MacBook-Pro.local>
2016-06-14 12:05:19 +0200

    Day12Morning

---
+++ Day12/todolistangular.js
@@ -0,0 +1,15 @@
+var app=angular.module("TodoApp", []);
+
+app.controller("MainCtrl", function ($scope) {
+	$scope.newItem="hi";
+	$scope.items=[];
+
+	$scope.addItem=function(){
+		$scope.items.push($scope.newItem);
+		$scope.newItem="";
+	};
+
+	$scope.remove=function(item) {
+		$scope.items.splice(item, 1);
+	}
+});
